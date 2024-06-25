14a58a97ff5299f4f540b1562f44b3a606a8670f
Malcom X <Taajlogan@gmail.com>
2024-06-25 17:19:09 -0400

    added 2  more category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const { Category, Product, Tag } = require('../../models');
+const { json } = require('body-parser');
+const { error } = require('console');
 // The `/api/categories` endpoint
 
 
@@ -12,19 +15,45 @@ router.get('/', async(req, res) => {
     return res.status(200).json(categoryData);
   } catch (error) {
     console.error(error);
-    return res.status(500).send("Internal Service Error api/category get route");
+    return res.status(500).send("Internal Service Error: api/category get route");
   }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
 // find one category by its `id` value
+router.get('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    return res.status(200).json(categoryData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Service Error: Unable to retrieve category.");
+  }
 
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// create a new category
+router.post('/', async(req, res) => {
+  try {
+    const [affectedRows, affectedCategories] = await Category.update({
+      category_name: req.body.category_name,
+       },
+       {
+      where: {
+        id: req.params.id, 
+      },
+    },
+  );
+  if (affectedRows === 0) {
+    return res.status(404).send("Category not found.");
+    } 
+    return res.status(200),json(affectedCategories[0]);
+  } catch (error) {
+    return res.status(500).send("Internal Service Error: Unable to update category");
+  }
 });
 
 router.put('/:id', (req, res) => {
