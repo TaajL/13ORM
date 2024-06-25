d968a253a6132c7657af11d7b38253c344e4bba6
Malcom X <Taajlogan@gmail.com>
2024-06-25 14:15:08 -0400

    joined tables in modelIndexjs

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categoryId',
+  onDelete: ' CASCADE',
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'categoryId',
+  onDelete: ' CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  as: 'tagproducts',
+});
 
 module.exports = {
   Product,
